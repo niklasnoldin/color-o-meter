5cc4eae2c6bcefa35f7fdb35d29368831f7c6e3f
Niklas Noldin <niklasnoldin@Niklass-MacBook-Pro.local>
2018-10-11 23:38:33 +0200

    actually works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import greeting from './greeting';
+import convert from "color-convert";
+import "./sass/index.scss";
 
-require('./sass/index.scss');
+const colorPicker = document.querySelector("#colorPicker");
 
-// example code
-var messages = ['Hi', 'from', 'Webpack'];
+function GetColor() {
+  let hexValue = colorPicker.value;
+  console.log(hexValue.substring(1, 7));
+  return hexValue.substring(1, 7);
+}
 
-messages.map(message => {
-  console.log(`${message}`);
+function ConvertHexToHue(hexCode) {
+  return convert.hex.hsv(hexCode)[0];
+}
+
+function ConvertHueToLength(hueDeg) {
+  return 620 - (170 / 270) * hueDeg;
+}
+
+function InsertIntoHtml(hexValue) {
+  let value = ConvertHueToLength(ConvertHexToHue("B000B5"));
+  document.querySelector("#result").innerHTML = value;
+}
+
+colorPicker.addEventListener("change", ev => {
+  document.querySelector("result").innerHTML = ConvertHueToLength(
+    ConvertHexToHue(GetColor())
+  );
 });
 
-console.log(greeting('Niklas'));
+InsertIntoHtml(GetColor());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: { main: "./src/index.js" },
   output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
@@ -17,34 +17,34 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['babel-preset-env'],
-          },
-        },
+            presets: ["babel-preset-env"]
+          }
+        }
       },
       // use sass for all .scss files
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.svg$/,
-        use: 'file-loader',
-      },
-    ],
+        use: "file-loader"
+      }
+    ]
   },
 
   plugins: [
     // clean dist folder before each build
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
 
     // use index.html as a template
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'false',
-    }),
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "false"
+    })
   ],
 
   optimization: {
@@ -52,8 +52,8 @@ module.exports = {
       // minify js
       new UglifyJSPlugin({
         cache: true,
-        parallel: true,
-      }),
-    ],
-  },
+        parallel: true
+      })
+    ]
+  }
 };
