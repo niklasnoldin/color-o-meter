705a528f73bdf1df6d5bcb5514fc6002f1910e0d
Niklas Noldin <niklasnoldin@Niklass-MacBook-Pro.local>
2018-10-12 00:26:29 +0200

    now really somehow works 4 real

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import convert from "color-convert";
 import "./sass/index.scss";
 
 const colorPicker = document.querySelector("#colorPicker");
-
+console.log(colorPicker);
+const heightInput = document.querySelector("#heightInput");
 function GetColor() {
   let hexValue = colorPicker.value;
-  console.log(hexValue.substring(1, 7));
   return hexValue.substring(1, 7);
 }
 
@@ -14,18 +14,37 @@ function ConvertHexToHue(hexCode) {
 }
 
 function ConvertHueToLength(hueDeg) {
-  return 620 - (170 / 270) * hueDeg;
+  return Math.round((620 - (170 / 270) * hueDeg) * 100) / 100;
 }
 
 function InsertIntoHtml(hexValue) {
-  let value = ConvertHueToLength(ConvertHexToHue("B000B5"));
+  let value = ConvertHueToLength(ConvertHexToHue(hexValue));
   document.querySelector("#result").innerHTML = value;
+  document.querySelector("#resultText").classList.add("hidden");
 }
 
 colorPicker.addEventListener("change", ev => {
-  document.querySelector("result").innerHTML = ConvertHueToLength(
-    ConvertHexToHue(GetColor())
-  );
+  changeHandle();
 });
 
 InsertIntoHtml(GetColor());
+
+heightInput.addEventListener("keyup", () => {
+  changeHandle();
+});
+
+function changeHandle() {
+  let height = heightInput.value;
+  console.log(height);
+  if (height.length != 0) {
+    document.querySelector("#resultText").classList.remove("hidden");
+    let colorLength = ConvertHueToLength(ConvertHexToHue(GetColor()));
+    let heightResult = document.querySelector("#heightResult");
+    let colorResult = document.querySelector("#colorResult");
+    colorResult.innerHTML = "#" + GetColor();
+    heightResult.innerHTML =
+      Math.round((height / colorLength / 1e-7) * 100) / 100;
+  } else {
+    document.querySelector("#resultText").classList.add("hidden");
+  }
+}
