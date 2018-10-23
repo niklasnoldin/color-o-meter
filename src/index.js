009584150984bbaa3c0fb9accdf14df3ea7d10c4
Niklas Noldin <niklas.noldin@gmail.com>
2018-10-23 13:46:15 +0200

    dist

--- src/index.js
+++ src/index.js
@@ -2,53 +2,42 @@ import convert from "color-convert";
 import "./sass/index.scss";
 
 const colorPicker = document.querySelector("#colorPicker");
-console.log(colorPicker);
+
 const heightInput = document.querySelector("#heightInput");
 
-function GetColor() {
-  let hexValue = colorPicker.value;
-  return hexValue.substring(1, 7);
-}
-
-function ConvertHexToHue(hexCode) {
-  return convert.hex.hsv(hexCode)[0];
-}
-
-function ConvertHueToLength(hueDeg) {
-  return Math.round((620 - (170 / 270) * hueDeg) * 100) / 100;
-}
-
-function InsertIntoHtml(hexValue) {
-  let value = ConvertHueToLength(ConvertHexToHue(hexValue));
-  document.querySelector("#result").innerHTML = value;
-  document.querySelector("#resultText").classList.add("hidden");
-}
-
-colorPicker.addEventListener("change", ev => {
-  changeHandle();
-});
-
-InsertIntoHtml(GetColor());
-
-heightInput.addEventListener("keyup", () => {
-  changeHandle();
-});
-
 function changeHandle() {
-  let value = ConvertHueToLength(ConvertHexToHue(GetColor()));
-  document.querySelector("#result").innerHTML = value;
-
+  let wavelength = convertHueToLength(convertHexToHue(getColor()));
   let height = heightInput.value;
-  console.log(height);
+
+  document.querySelector("#result").innerHTML = wavelength;
+
   if (height.length != 0) {
-    document.querySelector("#resultText").classList.remove("hidden");
-    let colorLength = ConvertHueToLength(ConvertHexToHue(GetColor()));
     let heightResult = document.querySelector("#heightResult");
     let colorResult = document.querySelector("#colorResult");
-    colorResult.innerHTML = "#" + GetColor();
+    document.querySelector("#resultText").classList.remove("hidden");
+    colorResult.innerHTML = "#" + getColor();
     heightResult.innerHTML =
-      Math.round((height / colorLength / 1e-7) * 100) / 100;
+      Math.round((height / wavelength / 1e-7) * 100) / 100;
   } else {
     document.querySelector("#resultText").classList.add("hidden");
   }
 }
+
+function getColor() {
+  let hexValue = colorPicker.value;
+  return hexValue.substring(1, 7);
+}
+
+function convertHexToHue(hexCode) {
+  return convert.hex.hsv(hexCode)[0];
+}
+
+function convertHueToLength(hueDeg) {
+  return Math.round((620 - (170 / 270) * hueDeg) * 100) / 100;
+}
+
+colorPicker.addEventListener("change", () => changeHandle());
+
+heightInput.addEventListener("keyup", () => changeHandle());
+
+changeHandle();
